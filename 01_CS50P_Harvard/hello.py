e1d5af536c3833fc45616709502a9539e8464421
Daria Frolova <frenigonda@gmail.com>
2024-03-01 17:38:41 +0000

    01: completed hello world

--- 01_CS50P_Harvard/hello.py
+++ 01_CS50P_Harvard/hello.py
@@ -1,6 +1,29 @@
 # Ask user for their name
 name = input("What's your name? ")
 
+# Remove whitespaces
+name = name.strip()
+
+# Capitalize name
+# name = name.capitalize()
+
+# Capitalize all words in a str
+name = name.title()
+
+# We can chain methods
+# name = input("What's your name? ").strip().title()
+
 # Say hello to user
 print("Hello, " + name)
 print("Hello,", name)
+print(f"Hello, {name}")
+
+print("Hello, ", end="")
+print(name)
+
+print("Hello,", name, sep=" ")
+
+# Split name into first name and last name
+first, last = name.split()
+
+print(f"Hello, {last}")
