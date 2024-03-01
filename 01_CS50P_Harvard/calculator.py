2f00827c8c5a8d7936582bee59acb483f4ead6a3
Daria Frolova <frenigonda@gmail.com>
2024-03-01 17:59:54 +0000

    01: simple calculator that takes floats

--- 01_CS50P_Harvard/calculator.py
+++ 01_CS50P_Harvard/calculator.py
@@ -1,5 +1,17 @@
-x = 1
-y = 2
-z = x + y
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
 
+# Still works, the result Z is float
+# x = float(input("What's x? "))
+# y = int(input("What's y? "))
+# z = x + y
+
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# Round to 1 digit after the floating point
+z = round(x + y, 1)
 print(z)
+
+# Alternatively we can format the result string
+# print(f"{z:.1f}")
